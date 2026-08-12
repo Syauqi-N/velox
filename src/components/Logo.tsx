daf9be7a421fc2c6e73adf2cd1943f8130dba94f
Syauqi-N <syauqi@dev.com>
2026-08-13 02:54:26 +0700

    style: enlarge VELOX logo in header
    - Desktop horizontal logo: 180x44 -> 220x56 (h-13)
    - Mobile compact logo: 40x40 -> 48x48 (h-12)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,18 +8,18 @@ export default function Logo({ href = "/" }: { href?: string }) {
       <Image
         src="/logos/velox-primary-horizontal.png"
         alt="Velox Capital"
-        width={180}
-        height={44}
-        className="hidden h-10 w-auto object-contain sm:block"
+        width={220}
+        height={56}
+        className="hidden h-13 w-auto object-contain sm:block"
         priority
       />
       {/* Mobile: compact logo */}
       <Image
         src="/logos/velox-primary-compact.png"
         alt="Velox Capital"
-        width={40}
-        height={40}
-        className="h-10 w-auto object-contain sm:hidden"
+        width={48}
+        height={48}
+        className="h-12 w-auto object-contain sm:hidden"
         priority
       />
     </Link>
